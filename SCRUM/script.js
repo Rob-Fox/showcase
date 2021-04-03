06906dd79044ece8c37f970a0038614481615707
Robert Fox <pv2fox@gmail.com>
2021-04-02 21:36:38 -0700

    added drag and drop functionality to tasks

--- SCRUM/script.js
+++ SCRUM/script.js
@@ -1,5 +1,26 @@
+taskNum = 1;
+function on_drag_start(e){
+    e.dataTransfer.setData('text/plain', e.target.id);
+
+    e.currentTarget.style.backgroundColor = 'white';
+}
+function on_dragover(e){
+    e.preventDefault();
+}
+function on_drop(e){
+    const id = e.dataTransfer.getData('text');
+    console.log(e.dataTransfer);
+    const draggableElement = document.getElementById(id);
+    console.log(draggableElement);
+    const dropzone = e.target;
+    dropzone.appendChild(draggableElement);
+    e.dataTransfer.clearData();
+}
+
+
 window.onload = function(){
     var list = document.getElementsByClassName('add');
+    
     function add_item(id, button){
             let item = document.createElement("textarea");
             button.style.display = "none";
@@ -14,6 +35,10 @@ window.onload = function(){
                     var div = document.createElement('div');
                     div.innerHTML = input.value;
                     div.setAttribute('class', 'task');
+                    div.setAttribute('draggable', 'true');
+                    div.setAttribute('ondragstart', 'on_drag_start(event)');
+                    div.setAttribute('id', 'task'+taskNum);
+                    taskNum++;
                     input.replaceWith(div);
                     button.style.display = "inline";
                 }
