091c8b3d7fb3062c1cfd74ea22e6e562ae9d2cc9
Rob-Fox <pv2fox@gmail.com>
2021-04-21 16:49:38 -0700

    added redirect if user tries to access SCRUM without logging in

--- showcase/SCRUM/views.py
+++ showcase/SCRUM/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 # from django.http import HttpResponse
 # from django.template import loader
 from .models import *
@@ -7,6 +7,8 @@ from login_reg.models import User
 # Create your views here.
 # def index(req, *arg_data, **kwarg_data):
 def index(req):
+    if req.session.get('user') == None:
+        return redirect('login_reg:index') 
     context = {
         'taskList': Task.objects.filter(creator=User.objects.get(id=req.session['user']))
     }
